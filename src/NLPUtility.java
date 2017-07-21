348a5e0dfb9b72bf57c378b00ca207c5c68ab581
pkalluri <pkalluri@mit.edu>
2017-07-21 05:55:48 +0200

    Created demo of system on NL Macbeth

--- src/NLPUtility.java
+++ src/NLPUtility.java
@@ -1,11 +1,13 @@
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.lang.reflect.Field;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.process.CoreLabelTokenFactory;
 import edu.stanford.nlp.process.PTBTokenizer;
 import edu.stanford.nlp.process.Tokenizer;
@@ -18,7 +20,7 @@ public class NLPUtility {
 	
 
 	/***
-	 * Get list of ActionEVents from the given lines.
+	 * Get list of ActionEvents from the given lines.
 	 * @param lines
 	 * @return
 	 */
@@ -37,12 +39,12 @@ public class NLPUtility {
 			line = line.replaceAll("(([A-Z][a-z]+)(?=\\s[A-Z])(?:\\s([A-Z][a-z]+))+)", "$2$3");
 
 			List<TypedDependency> tdl = NLPUtility.getTypedDependencyList(line, tokenizerFactory, parser, gsf);
-//					System.out.println("sentence: " + line);
+					System.out.println("Sentence: " + line);
 //					System.out.println(tdl);
 			try {
-				ActionEvent actionEvent = NLPUtility.getActionEvent(tdl);
-//						System.out.println("***" + actionEvent);
-				actionEvents.add(actionEvent);
+				List<ActionEvent> someActionEvents = NLPUtility.getActionEventsFromTDL(tdl);
+				System.out.println("Extracted events: " + someActionEvents);
+				actionEvents.addAll(someActionEvents);
 			} catch (FailedActionEventCreationException e) {
 				//Skip this line
 			}
@@ -55,8 +57,12 @@ public class NLPUtility {
 	 * @param tdl
 	 * @return
 	 * @throws FailedActionEventCreationException 
+	 * @throws IllegalAccessException 
+	 * @throws IllegalArgumentException 
 	 */
-	public static ActionEvent getActionEvent(List<TypedDependency> tdl) throws FailedActionEventCreationException {
+	public static List<ActionEvent> getActionEventsFromTDL(List<TypedDependency> tdl) throws FailedActionEventCreationException, IllegalArgumentException {
+		List<ActionEvent> actionEvents = new ArrayList<ActionEvent>();
+		
 		String action = null;
 		String actor = null;
 		String actedUpon = null;
@@ -64,48 +70,131 @@ public class NLPUtility {
 //		IndexedWord possibleAction;
 //		IndexedWord possibleActor;
 //		IndexedWord possibleActedUpon;
-		
-		for (TypedDependency td : tdl) {
+//		boolean lastLoop = false;
+//		while (!lastLoop) { //loop through tdl until tdl does not provide updates
+//			lastLoop = true; //possibly
+//		}
+		boolean firstSubject = true;
+
+		for (TypedDependency td : tdl) { //loop through all
 			GrammaticalRelation reln = td.reln();
-			String relnName = td.reln().getShortName();
-//			System.out.println(relnName);
+//			IndexedWord gov = td.gov();
+//			
+//			System.out.print(reln.getShortName());
+//			System.out.println();
+//			System.out.print(reln.getLongName());
+//			System.out.println();
+//			System.out.print(reln.getParent());
+//			System.out.println();
+//			for (Field field : gov.getClass().getDeclaredFields()) {
+//			    field.setAccessible(true);
+//			    String name = field.getName(); // 
+//			    Object value = null;
+//				try {
+//					value = field.get(gov);
+//				} catch (IllegalAccessException e) {
+//					e.printStackTrace();
+//				}
+//			    System.out.printf("Field name: %s, Field value: %s%n", name, value);
+//			}
 			
-			if (relnName.equals("nsubj")) { //found actor and action
-				if (action==null) { //only first time encountered
+//			String relnShortName = td.reln().getShortName();
+			String relnLongName = td.reln().getLongName();
+
+			
+//			if (relnName.equals("nsubj")) { //found actor and action
+//				if (action==null) { //only first time encountered
+//					actor = td.dep().originalText();
+//					action = td.gov().originalText();
+//				} else { //ignore
+//					break; //be done
+//				}
+//			}
+//			if (relnName.matches("compound")) { //ignore later parts of compound sentences
+//				break; //be done
+//			}
+//			if (relnName.matches("(nmod.*)")) { //found possible actedUpon
+//				if (actedUpon==null) { //only then consider filling
+//					String specific = reln.getSpecific();
+//					if (specific!=null && reln.getSpecific().matches("at")){
+//						//ignore
+//					} else {
+//						actedUpon = td.dep().originalText();
+//					}
+//				}
+//			}
+//			if (relnName.matches("(dobj)")) { //found possible actedUpon
+//				actedUpon = td.dep().originalText();
+//			}
+//			if (relnName.matches("(xcomp)")) { //multi-part action
+//				action += td.dep().originalText();
+//			}
+			
+			if (relnLongName.equals("nominal subject")) { //found actor and action
+				if (firstSubject == true) { //only first time encountered
+//				if (action!=null && actor!=null) {
+//					actionEvents.add(new ActionEvent(actor, action, actedUpon)); 
+//				}
 					actor = td.dep().originalText();
 					action = td.gov().originalText();
+					firstSubject = false; //update
 				} else { //ignore
 					break; //be done
 				}
-			}
-			if (relnName.matches("compound")) { //ignore later parts of compound sentences
+			} else if (relnLongName.equals("direct object")) { //found possible actedUpon
+				actedUpon = td.dep().originalText();
+			} else if (relnLongName.equals("copula")) { //multi-part action
+				action = td.dep().originalText() + getInitCaps(action);
+			}else if (relnLongName.equals("xclausal complement")) {
+				action += NLPUtility.getInitCaps( td.dep().originalText() );
+			} else if (relnLongName.equals("controlling nominal subject")) {
+				if (td.dep().originalText().equals(actor)){
+//					action += NLPUtility.getInitCaps(td.dep().originalText());
+				} else { //ignore
+					actedUpon = null;
+					break; //be done
+				}
+				
+			} else if (relnLongName.equals("coordination") || relnLongName.equals("compound modifier")) {
+//				if (action!=null && actor!=null) {
+////					actionEvents.add(new ActionEvent(actor, action, actedUpon)); 
+//				}
+//				actor = null;
+//				action = null;
+//				actedUpon = null;
 				break;
+			}else if (relnLongName.equals("marker")) {
+//				System.out.println(reln.getSpecific());
 			}
-			if (relnName.matches("(nmod.*)")) { //found possible actedUpon
-				if (actedUpon==null) { //only then consider filling
+			
+			if (actedUpon==null) { //check more things if still looking for actedUpon
+				if (relnLongName.equals("possession modifier") ) { //e.g. A is B's wife
+					action += "Of";
+					actedUpon = td.dep().originalText();
+				} else if (relnLongName.equals("nmod_preposition")) { //e.g. A is an enemy of B
 					String specific = reln.getSpecific();
 					if (specific!=null && reln.getSpecific().matches("at")){
 						//ignore
 					} else {
+						action += getInitCaps(reln.getSpecific());
 						actedUpon = td.dep().originalText();
 					}
 				}
-			}
-			if (relnName.matches("(dobj)")) { //found possible actedUpon
-				actedUpon = td.dep().originalText();
-			}
-			if (relnName.matches("(xcomp)")) { //multi-part action
-				action += td.dep().originalText();
-			}
-		}
-		if (action==null || actor==null) {
-			throw new FailedActionEventCreationException();
+			}//done checking more things
+		} //done parsing tdl
+		if (action!=null && actor!=null) {
+			actionEvents.add(new ActionEvent(actor, action, actedUpon)); 
 		}
 		
 		//build it
-		return new ActionEvent(actor, action, actedUpon);
+		return actionEvents;
 	}
 	
+	private static String getInitCaps(String str) {
+		// TODO Auto-generated method stub
+		return str.substring(0, 1).toUpperCase() + str.substring(1);
+	}
+
 	/***
 	 * Get typed dependency list for the given sentence, using the given general NLP tools.
 	 * @param sentence
@@ -133,7 +222,11 @@ public class NLPUtility {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws URISyntaxException, IOException {
-		String SCENARIO_NL_FILE = "Macbeth-Sentences.txt";
+		String SCENARIO_NL_FILE = "files/Macbeth-NL.txt";
+		@SuppressWarnings("unused")
 		Scenario scenario = FileUtility.getScenarioFromNLFile(SCENARIO_NL_FILE);
+//		for (ActionEvent a : scenario.actionEvents) {
+//			System.out.println(a);
+//		}
 	}
 }
